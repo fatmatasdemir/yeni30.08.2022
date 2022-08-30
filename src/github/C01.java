72a3cbfda57c0ff58686931c79f6d0bc0ec08cb0
Fatma Tasdemir <f.tasdemir571@gmail.com>
2022-08-30 13:46:53 +0200

    Version1

---
+++ src/github/C01.java
@@ -0,0 +1,34 @@
+package github;
+
+import java.util.Scanner;
+
+public class C01 {
+
+    Scanner scanner = new Scanner(System.in);
+    //  System.out.println("lutfen bir cumle girin");
+
+    String cumle = scanner.nextLine();
+    String kelime[] = cumle.split(" ");
+
+    int kelimeSayisi = 1;
+    //  for (int i = 0; i <cumle.length() ; i++) {
+    //if(Arrays.toString(kelime).substring(i,i+1).contains(",")){
+    // kelimeSayisi++;
+
+
+
+    // Githuba VERI gonderme
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
