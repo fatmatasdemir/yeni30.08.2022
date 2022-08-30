5e623fcad3e81fa2ec901fd497fe8a0ab2e845a3
Mihrican Pürbudak <mihricanpurbudak5@gmail.com>
2022-08-30 14:55:36 +0200

    BRANCH

--- src/githup/C02.java
+++ src/githup/C02.java
@@ -4,4 +4,8 @@ public class C02 {
     public static void main(String[] args) {
         System.out.println("Mihrican ve fatma githup calisiyor");
     }
+
+     {
+        System.out.println("Mihrican ve fatma githup calisiyor");
+    }
 }
