8e90eabf3bb60c9a0b28c903dbba326505ac1a28
Fatma Tasdemir <f.tasdemir571@gmail.com>
2022-08-30 13:19:37 +0200

    LOCAL

---
+++ src/github.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class github {
+
+     // GITHUBA VERI GONDERME
+    Scanner scanner = new Scanner(System.in);
+      //  System.out.println("lutfen bir cumle girin");
+
+    String cumle = scanner.nextLine();
+    String kelime[] = cumle.split(" ");
+
+    int kelimeSayisi = 1;
+      //  for (int i = 0; i <cumle.length() ; i++) {
+        //if(Arrays.toString(kelime).substring(i,i+1).contains(",")){
+           // kelimeSayisi++;
+
+
+}
